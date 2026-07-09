2ecbf0c32e08237b4294bb0a197d0d298331fcfa
Richfield Dilosi <richiedilosi2003@gmail.com>
2026-07-09 07:57:46 +0100

    Update urls.py

--- src/config/urls.py
+++ src/config/urls.py
@@ -3,6 +3,8 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 
 urlpatterns = [
     path("", include("jobs.urls")),
+    # admin
+    # path("admin/", include("admin.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
